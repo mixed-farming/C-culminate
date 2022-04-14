03d9af7f2c5ef0b2a95678daee16354271895ab2
cornfields <94393300+cornfields@users.noreply.github.com>
2022-04-14 21:10:31 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h> /* has  sin(), abs(), and fabs() */
+
+
+int main(void)
+
+{
+    double interval;
+    int i;
+    for(i = 0; i <30; i++)
+    {
+        interval = i/10.0;
+        printf("sin( %lf ) = %lf \t", interval, fabs(sin(interval)));
+    }
+    printf("\n+++++++\n");
+    return 0;
+}
