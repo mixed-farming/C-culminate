e51194e0c10f644250435543e1239508a21a266b
mixed-farming <94393300+mixed-farming@users.noreply.github.com>
2022-06-22 15:53:54 +0530

    Create ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char i=65;
+    printf("%c\n",i);
+    printf("%d\n",i);
+    char n='A';
+    printf("%c\n",n);
+    printf("%d",n);
+    return 0;
+}
