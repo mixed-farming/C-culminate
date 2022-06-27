da3b4418d4613a49c5993e496de35cfa354c8772
mixed-farming <94393300+mixed-farming@users.noreply.github.com>
2022-06-27 14:28:20 +0530

    Create biodata_paragraph.c

---
+++ biodata_paragraph.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Manoj M Mallya.\nIn short \v you can call me Mannu/MMM.\nI am also called by the name Vasuki by some of my relatives.\nMy native place is Udupi.\nI live in Udupi.\nI am the second son of Madhukar and Jayashree.\nMayur is my elder brother.\n\nI was born on 16-02-2002.I studied in Indrali high school for about 12 years.Later I completed my pre-university courses in Poornaprajna college.\n\nI love to sleep, eat & code XDXDXD ---___---.I also like to play badminton during my leisure times.Numismatics,philately ,collection of beads,leaves,feathers etc are my childhood hobbies which are in practice even now :) ");
+    return 0;
+}
