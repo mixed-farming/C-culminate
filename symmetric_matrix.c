ae46b093a443a09ca59caaf74f3e747cd25f02f9
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-07-04 13:55:40 +0530

    Create symmetric_matrix.c

---
+++ symmetric_matrix.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m,n,i,j,a[100][100],flag=1;
+    printf("Enter dimension : \n");
+    scanf("%d %d",&m,&n);
+    if(m!=n)
+        printf("\n\nIT IS NOT A SQUARE MATRIX\n\n");
+    else
+    {
+        printf("\nEnter elements : \n");
+        for(i=0; i<m; i++)
+            for(j=0; j<n; j++)
+                scanf("%d",&a[i][j]);
+
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                if(a[i][j]!=a[j][i])
+                {
+                    flag=0;
+                    if(flag==0)
+                        break;
+                }
+            }
+        }
+
+        if(flag==1)
+        {
+            printf("The matrix is symmetric");
+        }
+        else
+        {
+            printf("The matrix is not symmetric");
+        }
+    }
+                return 0;
+}
