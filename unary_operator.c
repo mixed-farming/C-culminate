c7b7546927618bc260e465caa5dc677f30b9aca9
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-07-23 21:37:04 +0530

    Create unary_operator.c

---
+++ unary_operator.c
@@ -0,0 +1,12 @@
+//unary minus operator
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=25;
+    float b=-3.56;
+    printf("%d\n",-a);
+    printf("%f\n",-b);
+    return 0;
+}
