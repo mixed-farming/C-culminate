696b0d6b99884936500acd2a4855b4d293388dce
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-07-24 16:10:38 +0530

    Create *_square_pattern.c

---
+++ *_square_pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,n;
+    printf("Enter the length of side of the square : ");
+    scanf("%d",&n);
+
+    printf("\n");
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
