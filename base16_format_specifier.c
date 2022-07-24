023d32c51dd96db1d1936063dbeb511f708e27ba
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-07-24 15:44:14 +0530

    Create base16_format_specifier.c

---
+++ base16_format_specifier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter the decimal number which is to be converted to the hexadecimal form : ");
+    scanf("%d",&a);
+
+    printf("The hexadecimal value of %d is %x",a,a);
+    return 0;
+}
