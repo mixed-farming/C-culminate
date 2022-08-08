12e40bb67a396aeefffc48b8f33f4dce052fecb2
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-09 02:17:17 +0530

    Create relational_operators.c

---
+++ relational_operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=4,j=5,r;
+    r=i>j;                   //The result of logical operators is always either 0 (FALSE) or 1 (TRUE)
+    printf("%d",r);
+    return 0;
+}
