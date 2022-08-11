8f2266f0989c6d6cd9ded298713379962553d443
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-11 11:53:06 +0530

    Create hello_world_without_semicolon.c

---
+++ hello_world_without_semicolon.c
@@ -0,0 +1,27 @@
+//there are various ways to do this
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    if (printf("Hello world!\n")){}
+}
+
+
+/*
+
+#include<stdio.h>  
+ int main()    
+{    
+switch(printf("hello world")){}    
+return 0;  
+} 
+
+#include<stdio.h>  
+ int main()    
+{    
+   while(!printf("hello world")){}    
+return 0;  
+} 
+
+*/
