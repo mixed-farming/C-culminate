9a8fcb22cc786ca41437e8a9a0fd661ab8ba0815
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-16 00:15:46 +0530

    Create else_if_ladder.c

---
+++ else_if_ladder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    if (4>5)
+        printf("impossible\n");
+    else if (4<5)
+        printf("you are right\n");
+    else
+        printf("you are wrong");
+    printf("mmm is tired\n");
+    printf("good night manoj\n");
+    return 0;
+}
