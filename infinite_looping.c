69f68d29460d2e3ba7316fd4a7ba2b81b248fdef
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-16 00:16:52 +0530

    Create infinite_looping.c

---
+++ infinite_looping.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// infinte loops can be set in 2 ways
+
+int main()
+{
+    /*for(;;)
+    {
+        printf("*");
+    }*/
+    while(1)
+    {
+        printf("$");
+    }
+    return 0;
+}
+
