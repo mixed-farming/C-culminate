c9f021d2b31cd06029e2323ee20e1cbb24f03f84
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-18 20:07:04 +0530

    Create escape_sequences.c

---
+++ escape_sequences.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\a\n\thello coder\n");
+    return 0;
+}
+
+/*
+
+\a is a ring bell rung when the control reaches this white space
+\n advances the program to next line
+\t gives a tab space
+\b gives a backspace(won't work for some compilers)s
+
+*/
