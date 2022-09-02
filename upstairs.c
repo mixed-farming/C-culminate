55d638eb3584359b377ea65f4b486acf8fa26eb4
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-09-02 13:31:01 +0530

    Create upstairs.c

---
+++ upstairs.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+n=5 will print
+
+                * *
+                * *
+            * * * *
+            * * * *
+        * * * * * *
+        * * * * * *
+    * * * * * * * *
+    * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+
+*/
+
+int main()
+{
+    int s,n,k,i,g,j;
+    printf("Enter number of steps : ");
+    scanf("%d",&s);
+    n=s*2;
+    printf("\n\n");
+
+    for(i=1; i<=n; i++)
+    {
+        k = (i%2==1) ? i+1 : i ;// k is set to i+1 for every i and i+1
+
+        for(g=1;g<=n-k;g++)
+        {
+            if(g==n||g==n-1)
+            {
+                printf("");
+            }
+            else
+            {
+                printf(" ");
+            }
+            printf(" ");
+        }
+
+        for(j=1; j<=k; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
