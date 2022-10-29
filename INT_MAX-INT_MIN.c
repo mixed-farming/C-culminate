86e76383271b575386263a548f3d9102aa823564
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-10-29 11:33:36 +0530

    Create INT_MAX-INT_MIN.c

---
+++ INT_MAX-INT_MIN.c
@@ -0,0 +1,15 @@
+// C program to print values of INT_MAX and INT_MIN we have to include limits.h for results in C
+#include <limits.h>
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", INT_MAX);
+	printf("%d", INT_MIN);
+}
+
+/*output :
+2147483647
+-2147483648
+*/
+
+
