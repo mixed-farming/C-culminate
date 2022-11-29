749a2ba3c87a6b84b07f8911d7dacdcad18619ac
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-11-29 18:06:31 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,23 @@
+//program to find the square root of 1st 100 natural numbers
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int a;
+    double square_roots;
+
+    // the function sqrt expects a double data type argument so we convert int to a double.
+
+    printf("square roots of numbers from 1 to 100 are : \n\n");
+
+    for  (a=1 ;a<=100; a++)
+    {
+        square_roots = sqrt(a);
+
+        printf("\t sqrt(%d)=%lf \n" ,a , square_roots);
+    }
+    return 0;
+}
