1cc8d458a7e13032b636d8b7a2ba699f842e7922
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-11-30 12:20:43 +0530

    Update smallest_of_2_numbers.c

--- smallest_of_2_numbers.c
+++ smallest_of_2_numbers.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int *smallest(int *x,int *y)
+{
+    return(*x < *y ? x : y);
+}
+
+int main()
+{
+    int a,b;
+    int *p;
+    printf("Enter two numbers for comparison : \n");
+    scanf("%d %d",&a,&b);
+
+    p = smallest(&a,&b);
+    printf("\nThe smallest number is %d.\n",*p);
+    return 0;
+}
