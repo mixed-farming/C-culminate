a5d85702ac310a4d79197306a0f8434b801731b7
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-12-17 20:40:59 +0530

    Create *_zig-zag_RowCount.c

---
+++ *_zig-zag_RowCount.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+
+Basically prints the letter 'M' with specified number of rows
+input = 5
+output:
+
+    *       *
+   * *     * *
+  *   *   *   *
+ *     * *     *
+*       *       *
+
+*/
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows : ");
+    scanf("%d",&n);
+    
+    int length=2*n+(n-1)+(n-2);
+    int p=n,q=2*n+(n-2);//first_pointer,second_pointer
+    int s=0;//space_equivalence
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=length;j++)
+        {
+            if(j+s==p||j-s==p||j+s==q||j-s==q)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+        s++;
+    }
+    return 0;
+}
